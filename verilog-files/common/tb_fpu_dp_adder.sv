d27e0ef8870ea063e9f3379e42f3bcba842bff6c
ahmedemad81 <Ahmed.Younes01@eng-st.cu.edu.eg>
2022-12-31 01:39:25 +0200

    Adjust Add test benches

--- verilog-files/common/tb_fpu_dp_adder.sv
+++ verilog-files/common/tb_fpu_dp_adder.sv
@@ -1,120 +1,71 @@
-module tb_fpu_dp_adder();
+module tb_fpu_dp_adder #(parameter WIDTH=64);
+real A;
+real B;
+reg [63:0] result;
+wire overflow_underflow_flag;
+fpu_dp_adder Add ($realtobits (A), $realtobits (B), result,overflow_underflow_flag);
 
-    reg [63:0] a, b;
-    wire [63:0] c;
-    wire overflow_underflow_flag;
+initial  
+begin
+A = 4.20;  
+B = 3.20;  
+#20
+A = 0.66; 
+B = 0.51;  
+#20
+A = -6.40; 
+B = -0.50; 
+#20
+A = 6.40;  
+B = -0.50;  	
+#20
+A = 2.82; 
+B = -0.94; 
+#20
+A = 2234.0132;
+B = -1235.3412;
+#20
+A = 1.0132;
+B = -1235.3412;
+#20
+A = -0.0132;
+B = -1235.3412;
+#20
+A = 0.0152;
+B = -0.3412;
+#20
+A = 124054.4312345;
+B = -9213743.123655343;
+#20
+A= 121.3232;
+B= -123.1231;
+end
 
-    fpu_dp_adder uut (
-        .a(a),
-        .b(b),
-        .result(c),
-        .overflow_underflow_flag(overflow_underflow_flag)
-    );
-    real a_real, b_real, c_real, expected_c_real;
-    reg isCorrect;
-
-    initial begin
-        a_real = 1.0;
-        b_real = 2.0;
-        a = $realtobits(a_real);
-        b = $realtobits(b_real);
-        #10;
-        c_real = $bitstoreal(c);
-        expected_c_real = a_real + b_real;
-        isCorrect = (c_real == expected_c_real);
-        $display("Test #1: a = %f, b = %f, c = %f, expected_c = %f, isCorrect = %d", a_real, b_real, c_real, expected_c_real, isCorrect);
-
-        a_real = 2.0;
-        b_real = 1.0;
-        a = $realtobits(a_real);
-        b = $realtobits(b_real);
-        #10;
-        c_real = $bitstoreal(c);
-        expected_c_real = a_real + b_real;
-        isCorrect = (c_real == expected_c_real);
-        $display("Test #2: a = %f, b = %f, c = %f, expected_c = %f, isCorrect = %d", a_real, b_real, c_real, expected_c_real, isCorrect);
-
-        a_real = 1.0;
-        b_real = -2.0;
-        a = $realtobits(a_real);
-        b = $realtobits(b_real);
-        #10;
-        c_real = $bitstoreal(c);
-        expected_c_real = a_real + b_real;
-        isCorrect = (c_real == expected_c_real);
-        $display("Test #3: a = %f, b = %f, c = %f, expected_c = %f, isCorrect = %d", a_real, b_real, c_real, expected_c_real, isCorrect);
-
-        a_real = -1.0;
-        b_real = 2.0;
-        a = $realtobits(a_real);
-        b = $realtobits(b_real);
-        #10;
-        c_real = $bitstoreal(c);
-        expected_c_real = a_real + b_real;
-        isCorrect = (c_real == expected_c_real);
-        $display("Test #4: a = %f, b = %f, c = %f, expected_c = %f, isCorrect = %d", a_real, b_real, c_real, expected_c_real, isCorrect);
-
-        a_real = 121.3232;
-        b_real = 123.1231;
-        a = $realtobits(a_real);
-        b = $realtobits(b_real);
-        #10;
-        c_real = $bitstoreal(c);
-        expected_c_real = a_real + b_real;
-        isCorrect = (c_real == expected_c_real);
-        $display("Test #5: a = %f, b = %f, c = %f, expected_c = %f, isCorrect = %d", a_real, b_real, c_real, expected_c_real, isCorrect);
-
-        a_real = 121.3232;
-        b_real = -123.1231;
-        a = $realtobits(a_real);
-        b = $realtobits(b_real);
-        #10;
-        c_real = $bitstoreal(c);
-        expected_c_real = a_real + b_real;
-        isCorrect = (c_real == expected_c_real);
-        $display("Test #6: a = %f, b = %f, c = %f, expected_c = %f, isCorrect = %d", a_real, b_real, c_real, expected_c_real, isCorrect);
-
-        a_real = -121.3232;
-        b_real = 123.1231;
-        a = $realtobits(a_real);
-        b = $realtobits(b_real);
-        #10;
-        c_real = $bitstoreal(c);
-        expected_c_real = a_real + b_real;
-        isCorrect = (c_real == expected_c_real);
-        $display("Test #7: a = %f, b = %f, c = %f, expected_c = %f, isCorrect = %d", a_real, b_real, c_real, expected_c_real, isCorrect);
-
-        a_real = 37584.0132;
-        b_real = 123.1231;
-        a = $realtobits(a_real);
-        b = $realtobits(b_real);
-        #10;
-        c_real = $bitstoreal(c);
-        expected_c_real = a_real + b_real;
-        isCorrect = (c_real == expected_c_real);
-        $display("Test #8: a = %f, b = %f, c = %f, expected_c = %f, isCorrect = %d", a_real, b_real, c_real, expected_c_real, isCorrect);
-
-        a_real = 0.002112;
-        b_real = -0.002132;
-        a = $realtobits(a_real);
-        b = $realtobits(b_real);
-        #10;
-        c_real = $bitstoreal(c);
-        expected_c_real = a_real + b_real;
-        isCorrect = (c_real == expected_c_real);
-        $display("Test #9: a = %f, b = %f, c = %f, expected_c = %f, isCorrect = %d", a_real, b_real, c_real, expected_c_real, isCorrect);
-
-        a_real = 124054.4312345;
-        b_real = -9213743.123655343;
-        a = $realtobits(a_real);
-        b = $realtobits(b_real);
-        #10;
-        c_real = $bitstoreal(c);
-        expected_c_real = a_real + b_real;
-        isCorrect = (c_real == expected_c_real);
-        $display("Test #10: a = %f, b = %f, c = %f, expected_c = %f, isCorrect = %d", a_real, b_real, c_real, expected_c_real, isCorrect);
-	 
-        $finish;
-    end
-
+initial
+begin
+$display("Double Precision Addition\n");
+#15
+$display("A = %f, B = %f, Expected Value = %f, Result = %f\n",A,B,A+B,$bitstoreal(result));
+#20
+$display("A = %f, B = %f, Expected Value = %f, Result = %f\n",A,B,A+B,$bitstoreal(result));
+#20
+$display("A = %f, B = %f, Expected Value = %f, Result = %f\n",A,B,A+B,$bitstoreal(result));
+#20
+$display("A = %f, B = %f, Expected Value = %f, Result = %f\n",A,B,A+B,$bitstoreal(result));
+#20
+$display("A = %f, B = %f, Expected Value = %f, Result = %f\n",A,B,A+B,$bitstoreal(result));
+#20
+$display("A = %f, B = %f, Expected Value = %f, Result = %f\n",A,B,A+B,$bitstoreal(result));
+#20
+$display("A = %f, B = %f, Expected Value = %f, Result = %f\n",A,B,A+B,$bitstoreal(result));
+#20
+$display("A = %f, B = %f, Expected Value = %f, Result = %f\n",A,B,A+B,$bitstoreal(result));
+#20
+$display("A = %f, B = %f, Expected Value = %f, Result = %f\n",A,B,A+B,$bitstoreal(result));
+#20
+$display("A = %f, B = %f, Expected Value = %f, Result = %f\n",A,B,A+B,$bitstoreal(result));
+#20
+$display("A = %f, B = %f, Expected Value = %f, Result = %f\n",A,B,A+B,$bitstoreal(result));
+$finish;
+end
 endmodule
--- verilog-files/common/tb_fpu_sp_adder.sv
+++ verilog-files/common/tb_fpu_sp_adder.sv
@@ -1,120 +1,71 @@
-module tb_fpu_sp_adder();
+module tb_fpu_sp_adder #(parameter WIDTH=32);
+shortreal A;
+shortreal B;
+reg [31:0] result;
+wire overflow_underflow_flag;
+fpu_sp_adder Add ($shortrealtobits (A), $shortrealtobits (B), result,overflow_underflow_flag);
 
-    reg [31:0] a, b;
-    wire [31:0] c;
-    wire overflow_underflow_flag;
+initial  
+begin
+A = 4.20;  
+B = 3.20;  
+#20
+A = 0.66; 
+B = 0.51;  
+#20
+A = -6.40; 
+B = -0.50; 
+#20
+A = 6.40;  
+B = -0.50;  	
+#20
+A = 2.82; 
+B = -0.94; 
+#20
+A = 2234.0132;
+B = -1235.3412;
+#20
+A = 1.0132;
+B = -1235.3412;
+#20
+A = -0.0132;
+B = -1235.3412;
+#20
+A = 0.0152;
+B = -0.3412;
+#20
+A = 124054.4312345;
+B = -9213743.123655343;
+#20
+A= 121.3232;
+B= -123.1231;
+end
 
-    fpu_sp_adder uut (
-        .a(a),
-        .b(b),
-        .result(c),
-        .overflow_underflow_flag(overflow_underflow_flag)
-    );
-    shortreal a_real, b_real, c_real, expected_c_real;
-    reg isCorrect;
-
-    initial begin
-        a_real = 1.0;
-        b_real = 2.0;
-        a = $shortrealtobits(a_real);
-        b = $shortrealtobits(b_real);
-        #10;
-        c_real = $bitstoshortreal(c);
-        expected_c_real = a_real + b_real;
-        isCorrect = (c_real == expected_c_real);
-        $display("Test #1: a = %f, b = %f, c = %f, expected_c = %f, isCorrect = %d", a_real, b_real, c_real, expected_c_real, isCorrect);
-
-        a_real = 2.0;
-        b_real = 1.0;
-        a = $shortrealtobits(a_real);
-        b = $shortrealtobits(b_real);
-        #10;
-        c_real = $bitstoshortreal(c);
-        expected_c_real = a_real + b_real;
-        isCorrect = (c_real == expected_c_real);
-        $display("Test #2: a = %f, b = %f, c = %f, expected_c = %f, isCorrect = %d", a_real, b_real, c_real, expected_c_real, isCorrect);
-
-        a_real = 1.0;
-        b_real = -2.0;
-        a = $shortrealtobits(a_real);
-        b = $shortrealtobits(b_real);
-        #10;
-        c_real = $bitstoshortreal(c);
-        expected_c_real = a_real + b_real;
-        isCorrect = (c_real == expected_c_real);
-        $display("Test #3: a = %f, b = %f, c = %f, expected_c = %f, isCorrect = %d", a_real, b_real, c_real, expected_c_real, isCorrect);
-
-        a_real = -1.0;
-        b_real = 2.0;
-        a = $shortrealtobits(a_real);
-        b = $shortrealtobits(b_real);
-        #10;
-        c_real = $bitstoshortreal(c);
-        expected_c_real = a_real + b_real;
-        isCorrect = (c_real == expected_c_real);
-        $display("Test #4: a = %f, b = %f, c = %f, expected_c = %f, isCorrect = %d", a_real, b_real, c_real, expected_c_real, isCorrect);
-
-        a_real = 121.3232;
-        b_real = 123.1231;
-        a = $shortrealtobits(a_real);
-        b = $shortrealtobits(b_real);
-        #10;
-        c_real = $bitstoshortreal(c);
-        expected_c_real = a_real + b_real;
-        isCorrect = (c_real == expected_c_real);
-        $display("Test #5: a = %f, b = %f, c = %f, expected_c = %f, isCorrect = %d", a_real, b_real, c_real, expected_c_real, isCorrect);
-
-        a_real = 121.3232;
-        b_real = -123.1231;
-        a = $shortrealtobits(a_real);
-        b = $shortrealtobits(b_real);
-        #10;
-        c_real = $bitstoshortreal(c);
-        expected_c_real = a_real + b_real;
-        isCorrect = (c_real == expected_c_real);
-        $display("Test #6: a = %f, b = %f, c = %f, expected_c = %f, isCorrect = %d", a_real, b_real, c_real, expected_c_real, isCorrect);
-
-        a_real = -121.3232;
-        b_real = 123.1231;
-        a = $shortrealtobits(a_real);
-        b = $shortrealtobits(b_real);
-        #10;
-        c_real = $bitstoshortreal(c);
-        expected_c_real = a_real + b_real;
-        isCorrect = (c_real == expected_c_real);
-        $display("Test #7: a = %f, b = %f, c = %f, expected_c = %f, isCorrect = %d", a_real, b_real, c_real, expected_c_real, isCorrect);
-
-        a_real = 37584.0132;
-        b_real = 123.1231;
-        a = $shortrealtobits(a_real);
-        b = $shortrealtobits(b_real);
-        #10;
-        c_real = $bitstoshortreal(c);
-        expected_c_real = a_real + b_real;
-        isCorrect = (c_real == expected_c_real);
-        $display("Test #8: a = %f, b = %f, c = %f, expected_c = %f, isCorrect = %d", a_real, b_real, c_real, expected_c_real, isCorrect);
-
-        a_real = 0.002112;
-        b_real = -0.002132;
-        a = $shortrealtobits(a_real);
-        b = $shortrealtobits(b_real);
-        #10;
-        c_real = $bitstoshortreal(c);
-        expected_c_real = a_real + b_real;
-        isCorrect = (c_real == expected_c_real);
-        $display("Test #9: a = %f, b = %f, c = %f, expected_c = %f, isCorrect = %d", a_real, b_real, c_real, expected_c_real, isCorrect);
-
-        a_real = 124054.4312345;
-        b_real = -9213743.123655343;
-        a = $shortrealtobits(a_real);
-        b = $shortrealtobits(b_real);
-        #10;
-        c_real = $bitstoshortreal(c);
-        expected_c_real = a_real + b_real;
-        isCorrect = (c_real == expected_c_real);
-        $display("Test #10: a = %f, b = %f, c = %f, expected_c = %f, isCorrect = %d", a_real, b_real, c_real, expected_c_real, isCorrect);
-	 
-        $finish;
-    end
-
+initial
+begin
+$display("Single Precision Addition\n");
+#15
+$display("A = %f, B = %f, Expected Value = %f, Result = %f\n",A,B,A+B,$bitstoshortreal(result));
+#20
+$display("A = %f, B = %f, Expected Value = %f, Result = %f\n",A,B,A+B,$bitstoshortreal(result));
+#20
+$display("A = %f, B = %f, Expected Value = %f, Result = %f\n",A,B,A+B,$bitstoshortreal(result));
+#20
+$display("A = %f, B = %f, Expected Value = %f, Result = %f\n",A,B,A+B,$bitstoshortreal(result));
+#20
+$display("A = %f, B = %f, Expected Value = %f, Result = %f\n",A,B,A+B,$bitstoshortreal(result));
+#20
+$display("A = %f, B = %f, Expected Value = %f, Result = %f\n",A,B,A+B,$bitstoshortreal(result));
+#20
+$display("A = %f, B = %f, Expected Value = %f, Result = %f\n",A,B,A+B,$bitstoshortreal(result));
+#20
+$display("A = %f, B = %f, Expected Value = %f, Result = %f\n",A,B,A+B,$bitstoshortreal(result));
+#20
+$display("A = %f, B = %f, Expected Value = %f, Result = %f\n",A,B,A+B,$bitstoshortreal(result));
+#20
+$display("A = %f, B = %f, Expected Value = %f, Result = %f\n",A,B,A+B,$bitstoshortreal(result));
+#20
+$display("A = %f, B = %f, Expected Value = %f, Result = %f\n",A,B,A+B,$bitstoshortreal(result));
+$finish;
+end
 endmodule
