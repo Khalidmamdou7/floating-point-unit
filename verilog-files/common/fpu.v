7f1d41279baea24efcafe9b414d7477d30aefaea
Khalid Mamdouh <khalidmamdou7@gmail.com>
2023-01-01 08:36:56 +0200

    update fpu and tb_fpu

--- verilog-files/common/fpu.v
+++ verilog-files/common/fpu.v
@@ -1,4 +1,5 @@
 module fpu(
+    input clk,
     input sp_dp,    // 0 = single precision, 1 = double precision
     input [1:0] opCode, // 00 = add, 01 = subtract, 10 = multiply, 11 = divide
     input [31:0] a_sp,
@@ -8,125 +9,51 @@ module fpu(
     output wire [31:0] result_sp,
     output wire [63:0] result_dp,
     output wire overflow,
-    output wire underflow
+    output wire underflow,
+    output wire ready
     );
 
-    wire [31:0] result_sp_adder;
-    wire [31:0] result_sp_subtractor;
-    wire [31:0] result_sp_multiplier;
-    wire [31:0] result_sp_divider;
 
-    wire [63:0] result_dp_adder;
-    wire [63:0] result_dp_subtractor;
-    wire [63:0] result_dp_multiplier;
-    wire [63:0] result_dp_divider;
-
-    wire overflow_underflow_flag_adder_sp;
-    wire overflow_underflow_flag_subtractor_sp;
-    wire overflow_underflow_flag_adder_dp;
-    wire overflow_underflow_flag_subtractor_dp;
-    wire overflow_flag_multiplier_sp;
-    wire underflow_flag_multiplier_sp;
-    wire overflow_flag_multiplier_dp;
-    wire underflow_flag_multiplier_dp;
-    wire overflow_flag_divider_sp;
-    wire underflow_flag_divider_sp;
-    wire overflow_flag_divider_dp;
-    wire underflow_flag_divider_dp;
-
-
-    fpu_dp_adder adder (
-        .a(a_dp),
-        .b(b_dp),
-        .result(result_dp_adder),
-        .overflow_underflow_flag(overflow_underflow_flag_adder_dp)
-    );
-
-    fpu_dp_subtractor subtractor (
-        .a(a_dp),
-        .b(b_dp),
-        .result(result_dp_subtractor),
-        .overflow_underflow_flag(overflow_underflow_flag_subtractor_dp)
-    );
-
-    fpu_dp_multiplier multiplier (
-        .A(a_dp),
-        .B(b_dp),
-        .result(result_dp_multiplier),
-        .overflow(overflow_flag_multiplier_dp),
-        .underflow(underflow_flag_multiplier_dp)
-    );
-
-    fpu_dp_divider divider (
-        .A(a_dp),
-        .B(b_dp),
-        .result(result_dp_divider),
-        .overflow(overflow_flag_divider_dp),
-        .underflow(underflow_flag_divider_dp)
-    );
-
-    fpu_sp_adder sp_adder (
-        .a(a_sp),
-        .b(b_sp),
-        .result(result_sp_adder),
-        .overflow_underflow_flag(overflow_underflow_flag_adder_sp)
-    );
-
-    fpu_sp_subtractor sp_subtractor (
-        .a(a_sp),
-        .b(b_sp),
-        .result(result_sp_subtractor),
-        .overflow_underflow_flag(overflow_underflow_flag_subtractor_sp)
-    );
-
-    fpu_sp_multiplier sp_multiplier (
+    // Single precision
+    wire overflow_sp;
+    wire underflow_sp;
+    wire ready_sp;
+    wire [31:0] result_sp_temp;
+    
+    fpu_sp fpu_sp_inst (
+        .clk(clk),
+        .OpCode(opCode),
         .A(a_sp),
         .B(b_sp),
-        .result(result_sp_multiplier),
-        .overflow(overflow_flag_multiplier_sp),
-        .underflow(underflow_flag_multiplier_sp)
+        .Result(result_sp_temp),
+        .Ready(ready_sp),
+        .Overflow_out(overflow_sp),
+        .Underflow_out(underflow_sp)
     );
 
-    fpu_sp_divider sp_divider (
-        .A(a_sp),
-        .B(b_sp),
-        .result(result_sp_divider),
-        .overflow(overflow_flag_divider_sp),
-        .underflow(underflow_flag_divider_sp)
+    // Double precision
+    wire overflow_dp;
+    wire underflow_dp;
+    wire ready_dp;
+    wire [63:0] result_dp_temp;
+
+    fpu_dp fpu_dp_inst (
+        .clk(clk),
+        .OpCode(opCode),
+        .A(a_dp),
+        .B(b_dp),
+        .Result(result_dp_temp),
+        .Ready(ready_dp),
+        .Overflow_out(overflow_dp),
+        .Underflow_out(underflow_dp)
     );
 
-    assign result_sp = (opCode == 2'b00 && !sp_dp) ? result_sp_adder :
-                       (opCode == 2'b01 && !sp_dp) ? result_sp_subtractor :
-                       (opCode == 2'b10 && !sp_dp) ? result_sp_multiplier :
-                       (opCode == 2'b11 && !sp_dp) ? result_sp_divider :
-                       32'b0;
-    
-    assign result_dp = (opCode == 2'b00 && sp_dp) ? result_dp_adder :
-                          (opCode == 2'b01 && sp_dp) ? result_dp_subtractor :
-                          (opCode == 2'b10 && sp_dp) ? result_dp_multiplier :
-                          (opCode == 2'b11 && sp_dp) ? result_dp_divider :
-                          64'b0;
-    
-    assign overflow = (opCode == 2'b00 && !sp_dp) ? overflow_underflow_flag_adder_sp :
-                        (opCode == 2'b01 && !sp_dp) ? overflow_underflow_flag_subtractor_sp :
-                        (opCode == 2'b10 && !sp_dp) ? overflow_flag_multiplier_sp :
-                        (opCode == 2'b11 && !sp_dp) ? overflow_flag_divider_sp :
-                        (opCode == 2'b00 && sp_dp) ? overflow_underflow_flag_adder_dp :
-                        (opCode == 2'b01 && sp_dp) ? overflow_underflow_flag_subtractor_dp :
-                        (opCode == 2'b10 && sp_dp) ? overflow_flag_multiplier_dp :
-                        (opCode == 2'b11 && sp_dp) ? overflow_flag_divider_dp :
-                        1'b0;
-    
-    assign underflow = (opCode == 2'b00 && !sp_dp) ? overflow_underflow_flag_adder_sp :
-                        (opCode == 2'b01 && !sp_dp) ? overflow_underflow_flag_subtractor_sp :
-                        (opCode == 2'b10 && !sp_dp) ? underflow_flag_multiplier_sp :
-                        (opCode == 2'b11 && !sp_dp) ? underflow_flag_divider_sp :
-                        (opCode == 2'b00 && sp_dp) ? overflow_underflow_flag_adder_dp :
-                        (opCode == 2'b01 && sp_dp) ? overflow_underflow_flag_subtractor_dp :
-                        (opCode == 2'b10 && sp_dp) ? underflow_flag_multiplier_dp :
-                        (opCode == 2'b11 && sp_dp) ? underflow_flag_divider_dp :
-                        1'b0;
-    
+    // Select single or double precision
+    assign overflow = sp_dp ? overflow_dp : overflow_sp;
+    assign underflow = sp_dp ? underflow_dp : underflow_sp;
+    assign ready = sp_dp ? ready_dp : ready_sp;
 
-    
+    assign result_sp = sp_dp ? 32'b0 : result_sp_temp;
+    assign result_dp = sp_dp ? result_dp_temp : 64'b0;
+
 endmodule
--- verilog-files/common/tb_fpu.sv
+++ verilog-files/common/tb_fpu.sv
@@ -1,4 +1,5 @@
 module tb_fpu();
+localparam PERIOD = 10;
 
 shortreal a,b;
 real d,e,f;
@@ -9,7 +10,11 @@ reg [63:0] c64;
 reg overflow;
 reg underflow;
 
+reg clk;
+reg ready;
+
 fpu uut (
+    .clk(clk),
     .sp_dp(sp_dp), // 0 = single precision, 1 = double precision
     .opCode(op), // 0 = add, 1 = sub, 2 = mul, 3 = div
     .a_sp($shortrealtobits(a)),
@@ -19,64 +24,70 @@ fpu uut (
     .result_sp(c),
     .result_dp(c64),
     .overflow(overflow),
-    .underflow(underflow)
+    .underflow(underflow),
+    .ready(ready)
 );
 
+always begin
+	# (PERIOD/2) clk = ~clk;
+end
+
 initial begin
+    clk = 1'b0;
     // Test 1: add two single precision numbers
     sp_dp = 0;
     op = 0;
     a = 1.0;
     b = 2.0;
-    #1;
+    #(PERIOD*3)
     // Test 2: subtract two single precision numbers
     sp_dp = 0;
     op = 1;
     a = 1.0;
     b = 2.0;
-    #1;
+    #(PERIOD*3)
     // Test 3: multiply two single precision numbers
     sp_dp = 0;
     op = 2;
     a = 1.0;
     b = 2.0;
-    #1;
+    #(PERIOD*3)
     // Test 4: divide two single precision numbers
     sp_dp = 0;
     op = 3;
     a = 1.0;
     b = 2.0;
-    #1;
+    #(PERIOD*3)
     // Test 5: add two double precision numbers
     sp_dp = 1;
     op = 0;
     d = 2121121.123123;
     e = 2121221.123123;
-    #1;
+    #(PERIOD*3)
     // Test 6: subtract two double precision numbers
     sp_dp = 1;
     op = 1;
     d = 2121121.123123;
     e = 1.21;
-    #1;
+    #(PERIOD*3)
     // Test 7: multiply two double precision numbers
     sp_dp = 1;
     op = 2;
     d = 2121121.123123;
     e = 1212121.121212;
-    #1;
+    #(PERIOD*3)
     // Test 8: divide two double precision numbers
     sp_dp = 1;
     op = 3;
     d = 2121121.123123;
     e = 1212121.121212;
-    #1;
+    #(PERIOD*3)
     // Test 9: check overflow in adding two single precision numbers
     sp_dp = 0;
     op = 0;
     a = 1.2e38;
     b = 1.2e38;
-    #1;
+    #(PERIOD*3)
     // Test 10: check underflow in adding two single precision numbers
     sp_dp = 0;
     op = 0;
@@ -85,33 +96,38 @@ initial begin
 end
 
     initial begin
+        #(PERIOD*2)
+        $display("Testing single precision FPU");
         $display("Test 1: add two single precision numbers");
         $display("a = %f", a);
         $display("b = %f", b);
         $display("c = %f", $bitstoshortreal(c));
+        $display("c64 = %f", $bitstoreal(c64));
         $display("expected = %f", a+b);
         $display("overflow = %b", overflow);
         $display("underflow = %b", underflow);
         $display("Pass = %b", $bitstoshortreal(c) == a+b);
-        $1;
+        #(PERIOD*3)
         $display("Test 2: subtract two single precision numbers");
         $display("a = %f", a);
         $display("b = %f", b);
         $display("c = %f", $bitstoshortreal(c));
+        $display("c64 = %f", $bitstoreal(c64)); 
         $display("expected = %f", a-b);
         $display("overflow = %b", overflow);
         $display("underflow = %b", underflow);
         $display("Pass = %b", $bitstoshortreal(c) == a-b);
-        $1;
+        #(PERIOD*3)
         $display("Test 3: multiply two single precision numbers");
         $display("a = %f", a);
         $display("b = %f", b);
         $display("c = %f", $bitstoshortreal(c));
+        $display("c64 = %f", $bitstoreal(c64));
         $display("expected = %f", a*b);
         $display("overflow = %b", overflow);
         $display("underflow = %b", underflow);
         $display("Pass = %b", $bitstoshortreal(c) == a*b);
-        $1;
+        #(PERIOD*3)
         $display("Test 4: divide two single precision numbers");
         $display("a = %f", a);
         $display("b = %f", b);
@@ -119,8 +135,8 @@ end
         $display("expected = %f", a/b);
         $display("overflow = %b", overflow);
         $display("underflow = %b", underflow);
-        $display("Pass = %b", $bitstoshortreal(c) == a/b);
-        $1;
+        #(PERIOD*3)
+        $display("Testing double precision FPU");
         $display("Test 5: add two double precision numbers");
         $display("a = %f", d);
         $display("b = %f", e);
@@ -129,7 +145,7 @@ end
         $display("overflow = %b", overflow);
         $display("underflow = %b", underflow);
         $display("Pass = %b", $bitstoreal(c64) == d+e);
-        $1;
+        #(PERIOD*3)
         $display("Test 6: subtract two double precision numbers");
         $display("a = %f", d);
         $display("b = %f", e);
@@ -138,7 +154,7 @@ end
         $display("overflow = %b", overflow);
         $display("underflow = %b", underflow);
         $display("Pass = %b", $bitstoreal(c64) == d-e);
-        $1;
+        #(PERIOD*3)
         $display("Test 7: multiply two double precision numbers");
         $display("a = %f", d);
         $display("b = %f", e);
@@ -146,8 +162,7 @@ end
         $display("expected = %f", d*e);
         $display("overflow = %b", overflow);
         $display("underflow = %b", underflow);
-        $display("Pass = %b", $bitstoreal(c64) == d*e);
-        $1;
+        #(PERIOD*3)
         $display("Test 8: divide two double precision numbers");
         $display("a = %f", d);
         $display("b = %f", e);
@@ -155,8 +170,10 @@ end
         $display("expected = %f", d/e);
         $display("overflow = %b", overflow);
         $display("underflow = %b", underflow);
-        $display("Pass = %b", $bitstoreal(c64) == d/e);
-        $1;
+        #(PERIOD*3)
+        // $display("Intensive testing of all operations");
+        // $display("Intensive testing of addition of two single precision numbers");
+        // $display("Test 9: Adding two single precision numbers");
         $display("Test 9: check overflow in adding two single precision numbers");
         $display("a = %f", a);
         $display("b = %f", b);
@@ -165,7 +182,7 @@ end
         $display("overflow = %b", overflow);
         $display("underflow = %b", underflow);
         $display("Pass = %b", $bitstoshortreal(c) == a+b);
-        $1;
+        #(PERIOD*3)
         $display("Test 10: check underflow in adding two single precision numbers");
         $display("a = %f", a);
         $display("b = %f", b);
@@ -174,7 +191,7 @@ end
         $display("overflow = %b", overflow);
         $display("underflow = %b", underflow);
         $display("Pass = %b", $bitstoshortreal(c) == a+b);
-        $1;
+        #(PERIOD*3)
         $finish;
     end
 
