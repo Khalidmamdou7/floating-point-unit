89d950fac2c973bf5ac41d95db4086be24eb4bb4
Khalid Mamdouh <khalidmamdou7@gmail.com>
2023-01-02 00:07:55 +0200

    added adder_sp tests to main tb

--- verilog-files/common/tb_fpu.sv
+++ verilog-files/common/tb_fpu.sv
@@ -82,17 +82,49 @@ initial begin
     d = 2121121.123123;
     e = 1212121.121212;
     #(PERIOD*3)
-    // Test 9: check overflow in adding two single precision numbers
+    // (intensive test for adder sp)
     sp_dp = 0;
     op = 0;
-    a = 1.2e38;
-    b = 1.2e38;
+    a = 4.20;  
+    b = 3.20;  
     #(PERIOD*3)
-    // Test 10: check underflow in adding two single precision numbers
-    sp_dp = 0;
-    op = 0;
-    a = 1.2e-38;
-    b = -1.2e-38;
+    a = 0.66; 
+    b = 0.51;  
+    #(PERIOD*3)
+    a = -6.40; 
+    b = -0.50; 
+    #(PERIOD*3)
+    a = 6.40;  
+    b = -0.50;  	
+    #(PERIOD*3)
+    a = 2.82; 
+    b = -0.94; 
+    #(PERIOD*3)
+    a = 2234.0132;
+    b = -1235.3412;
+    #(PERIOD*3)
+    a = 1.0132;
+    b = -1235.3412;
+    #(PERIOD*3)
+    a = -0.0132;
+    b = -1235.3412;
+    #(PERIOD*3)
+    a = 0.0152;
+    b = -0.3412;
+    #(PERIOD*3)
+    a = 124054.4312345;
+    b = -9213743.123655343;
+    #(PERIOD*3)
+    a= 121.3232;
+    b= -123.1231;
+    #(PERIOD*3)
+    // Overflow Test
+    a= 2.21E38;
+    b= 2.21E38;
+    #(PERIOD*3)
+    // Underflow Test
+    a= 2.21E-38;
+    b = -2.2E-38;
 end
 
     initial begin
@@ -171,27 +203,151 @@ end
         $display("overflow = %b", overflow);
         $display("underflow = %b", underflow);
         #(PERIOD*3)
-        // $display("Intensive testing of all operations");
-        // $display("Intensive testing of addition of two single precision numbers");
-        // $display("Test 9: Adding two single precision numbers");
-        $display("Test 9: check overflow in adding two single precision numbers");
+        $display("Intensive testing of all operations");
+        $display("Intensive testing of addition of two single precision numbers");
         $display("a = %f", a);
         $display("b = %f", b);
         $display("c = %f", $bitstoshortreal(c));
+        $display("c64 = %f", $bitstoreal(c64));
         $display("expected = %f", a+b);
         $display("overflow = %b", overflow);
         $display("underflow = %b", underflow);
         $display("Pass = %b", $bitstoshortreal(c) == a+b);
         #(PERIOD*3)
-        $display("Test 10: check underflow in adding two single precision numbers");
         $display("a = %f", a);
         $display("b = %f", b);
         $display("c = %f", $bitstoshortreal(c));
+        $display("c64 = %f", $bitstoreal(c64));
         $display("expected = %f", a+b);
         $display("overflow = %b", overflow);
         $display("underflow = %b", underflow);
         $display("Pass = %b", $bitstoshortreal(c) == a+b);
         #(PERIOD*3)
+        $display("a = %f", a);
+        $display("b = %f", b);
+        $display("c = %f", $bitstoshortreal(c));
+        $display("c64 = %f", $bitstoreal(c64));
+        $display("expected = %f", a+b);
+        $display("overflow = %b", overflow);
+        $display("underflow = %b", underflow);
+        $display("Pass = %b", $bitstoshortreal(c) == a+b);
+        #(PERIOD*3)
+        $display("a = %f", a);
+        $display("b = %f", b);
+        $display("c = %f", $bitstoshortreal(c));
+        $display("c64 = %f", $bitstoreal(c64));
+        $display("expected = %f", a+b);
+        $display("overflow = %b", overflow);
+        $display("underflow = %b", underflow);
+        $display("Pass = %b", $bitstoshortreal(c) == a+b);
+        #(PERIOD*3)
+        $display("a = %f", a);
+        $display("b = %f", b);
+        $display("c = %f", $bitstoshortreal(c));
+        $display("c64 = %f", $bitstoreal(c64));
+        $display("expected = %f", a+b);
+        $display("overflow = %b", overflow);
+        $display("underflow = %b", underflow);
+        $display("Pass = %b", $bitstoshortreal(c) == a+b);
+        #(PERIOD*3)
+        $display("a = %f", a);
+        $display("b = %f", b);
+        $display("c = %f", $bitstoshortreal(c));
+        $display("c64 = %f", $bitstoreal(c64));
+        $display("expected = %f", a+b);
+        $display("overflow = %b", overflow);
+        $display("underflow = %b", underflow);
+        $display("Pass = %b", $bitstoshortreal(c) == a+b);
+        #(PERIOD*3)
+        $display("a = %f", a);
+        $display("b = %f", b);
+        $display("c = %f", $bitstoshortreal(c));
+        $display("c64 = %f", $bitstoreal(c64));
+        $display("expected = %f", a+b);
+        $display("overflow = %b", overflow);
+        $display("underflow = %b", underflow);
+        $display("Pass = %b", $bitstoshortreal(c) == a+b);
+        #(PERIOD*3)
+        $display("a = %f", a);
+        $display("b = %f", b);
+        $display("c = %f", $bitstoshortreal(c));
+        $display("c64 = %f", $bitstoreal(c64));
+        $display("expected = %f", a+b);
+        $display("overflow = %b", overflow);
+        $display("underflow = %b", underflow);
+        $display("Pass = %b", $bitstoshortreal(c) == a+b);
+        #(PERIOD*3)
+        $display("a = %f", a);
+        $display("b = %f", b);
+        $display("c = %f", $bitstoshortreal(c));
+        $display("c64 = %f", $bitstoreal(c64));
+        $display("expected = %f", a+b);
+        $display("overflow = %b", overflow);
+        $display("underflow = %b", underflow);
+        $display("Pass = %b", $bitstoshortreal(c) == a+b);
+        #(PERIOD*3)
+        $display("a = %f", a);
+        $display("b = %f", b);
+        $display("c = %f", $bitstoshortreal(c));
+        $display("c64 = %f", $bitstoreal(c64));
+        $display("expected = %f", a+b);
+        $display("overflow = %b", overflow);
+        $display("underflow = %b", underflow);
+        $display("Pass = %b", $bitstoshortreal(c) == a+b);
+        #(PERIOD*3)
+        $display("a = %f", a);
+        $display("b = %f", b);
+        $display("c = %f", $bitstoshortreal(c));
+        $display("c64 = %f", $bitstoreal(c64));
+        $display("expected = %f", a+b);
+        $display("overflow = %b", overflow);
+        $display("underflow = %b", underflow);
+        $display("Pass = %b", $bitstoshortreal(c) == a+b);
+        #(PERIOD*3)
+        $display("a = %f", a);
+        $display("b = %f", b);
+        $display("c = %f", $bitstoshortreal(c));
+        $display("c64 = %f", $bitstoreal(c64));
+        $display("expected = %f", a+b);
+        $display("overflow = %b", overflow);
+        $display("underflow = %b", underflow);
+        $display("Pass = %b", $bitstoshortreal(c) == a+b);
+        #(PERIOD*3)
+        $display("a = %f", a);
+        $display("b = %f", b);
+        $display("c = %f", $bitstoshortreal(c));
+        $display("c64 = %f", $bitstoreal(c64));
+        $display("expected = %f", a+b);
+        $display("overflow = %b", overflow);
+        $display("underflow = %b", underflow);
+        $display("Pass = %b", $bitstoshortreal(c) == a+b);
+        #(PERIOD*3)
+        $display("a = %f", a);
+        $display("b = %f", b);
+        $display("c = %f", $bitstoshortreal(c));
+        $display("c64 = %f", $bitstoreal(c64));
+        $display("expected = %f", a+b);
+        $display("overflow = %b", overflow);
+        $display("underflow = %b", underflow);
+        $display("Pass = %b", $bitstoshortreal(c) == a+b);
+        // $display("Test 9: check overflow in adding two single precision numbers");
+        // $display("a = %f", a);
+        // $display("b = %f", b);
+        // $display("c = %f", $bitstoshortreal(c));
+        // $display("expected = %f", a+b);
+        // $display("overflow = %b", overflow);
+        // $display("underflow = %b", underflow);
+        // $display("Pass = %b", $bitstoshortreal(c) == a+b);
+        // #(PERIOD*3)
+        // $display("Test 10: check underflow in adding two single precision numbers");
+        // $display("a = %f", a);
+        // $display("b = %f", b);
+        // $display("c = %f", $bitstoshortreal(c));
+        // $display("expected = %f", a+b);
+        // $display("overflow = %b", overflow);
+        // $display("underflow = %b", underflow);
+        // $display("Pass = %b", $bitstoshortreal(c) == a+b);
+        // #(PERIOD*3)
         $finish;
     end
 
